ddefc005d182c875be0ea0881c58b5c1414e2625
tlambrou <lambrou@gmail.com>
2017-10-09 09:03:41 -0700

    Fixed styles bug

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react'
 import './App.css'
 import Reactstrap from 'reactstrap';
-import './pk2-pro-html-v2/assets/css/paper-kit.css'
-import './pk2-pro-html-v2/assets/css/bootstrap.min.css'
-import './pk2-pro-html-v2/assets/css/nucleo-icons.css'
 import Home from './components/Home'
 import Profile from './components/Profile'
 import Data from './Data'
